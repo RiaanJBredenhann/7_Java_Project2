82c0f037b9a7148ad56414f154422ef1a4a7dc7e
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-22 13:18:57 +0200

    update

--- Book8/Project2_Listing1_1/src/project2/FileVisitorDemo.java
+++ Book8/Project2_Listing1_1/src/project2/FileVisitorDemo.java
@@ -4,17 +4,45 @@
  */
 package project2;
 
-/**
- *
- * @author Riaan HP
- */
+import java.nio.file.*;
+import java.io.*;
+import java.nio.file.attribute.*;
+
 public class FileVisitorDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Path start = Paths.get("c:\\Windows\\System32");
+        MyFileVisitor visitor = new MyFileVisitor();
+        
+        try
+        {
+            Files.walkFileTree(start, visitor);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
     
+    private static class MyFileVisitor extends SimpleFileVisitor <Path>
+    {
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attr)
+        {
+            System.out.println(file.toString());
+            return FileVisitResult.CONTINUE;
+        }
+        
+        public FileVisitResult visitFileFailed(Path file, BasicFileAttributes attr)
+        {
+            System.out.println(file.toString() + " COULD NOT ACCESS!");
+            return FileVisitResult.CONTINUE;
+        }
+        
+        public FileVisitResult preVisitDirectoryFailed(Path dir, IOException e)
+        {
+            System.out.println(dir.toString() + " COULD NOT ACCESS!");
+            return FileVisitResult.CONTINUE;
+        }
+    }
+  
 }
